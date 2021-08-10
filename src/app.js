5c3f5d37e01ead02c39b89cd9f2829bcc0e5aa6b
Manoj Pachuveedu <manoj@localhost.localdomain>
2021-08-11 00:56:49 +0530

    test

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ console.log(path.join(__dirname,'../public'));
 const app = express();
 app.set('view engine','hbs');
 app.use(express.static(path.join(__dirname,'../public')));
-
+//
 
 app.get("/help", ( req, res) => {
     res.send([{name: 'Manoj', age: 40},{name: 'Shru', age: 35}]);
@@ -25,7 +25,7 @@ app.get("/help", ( req, res) => {
  
  app.get("/", ( req, res) => {
     res.render('index',{title: 'myapp' , name: 'Manoj'});
- });
+ }); 
 
  
 app.listen(3000, () => { console.log('Server started on 3000')});
